54088361c2b3ecd9ad04dc31417a4a4f1782bc34
balaram <balarambk99@gmail.com>
2026-05-10 12:31:39 +0530

    changes add

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
 print("well come to my jungel iam kurady the bear and i will be your guide to  hello friends wellcome to wolrd")
+
+print("let us start our adventure in the jungle and explore the beauty of nature together")
